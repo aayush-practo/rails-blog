86e59a3a8d1a52dd37ffec311449b6f0dd980c05
aayush-practo <aayush.gupta@practo.com>
2016-08-09 10:59:07 +0000

    removing trackable properties

--- db/schema.rb
+++ db/schema.rb
@@ -36,11 +36,6 @@ ActiveRecord::Schema.define(version: 20160809063544) do
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
   end
