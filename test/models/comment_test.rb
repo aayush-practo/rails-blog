ea40d01b7ed2376993d80a04dbe3fde2be272fc8
aayush-practo <aayush.gupta@practo.com>
2016-08-09 13:18:29 +0000

    using annotate gem

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  commenter  :string(255)
+#  body       :text(65535)
+#  article_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
