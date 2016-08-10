18cb1a64a736eabfcba58e9bb3d52eae709e93a4
aayush-practo <aayush.gupta@practo.com>
2016-08-10 11:01:22 +0000

    adding pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 
   before_action :authenticate_user!
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 2)
   end
 
   def new
