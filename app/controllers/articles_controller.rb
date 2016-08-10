82c9350bdc12951963124ff6453b9677fc52cf6f
aayush-practo <aayush.gupta@practo.com>
2016-08-10 07:27:11 +0000

    replacing single quotes with double

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
     if @article.save
       redirect_to @article
     else
-      render 'new'
+      render "new"
     end
   end
 
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
     if @article.update(article_params)
       redirect_to @article
     else
-      render 'edit'
+      render "edit"
     end
   end
 
