598b2c312b177d185926eb98ccc5feece8fca9c6
aayush-practo <aayush.gupta@practo.com>
2016-08-11 12:40:46 +0000

    removing puts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,6 @@ class ArticlesController < ApplicationController
 
   before_action :authenticate_user!
   def index
-    puts "Params: #{params[:q]}"
     @q = Article.search(params[:q])
     @articles = @q.result.paginate(page: params[:page], per_page: 10)
   end
