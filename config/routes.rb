026e4c3da9dcad638dbf4458f487852b429c7f36
aayush-practo <aayush.gupta@practo.com>
2016-08-15 11:03:10 +0000

    removing conditional routes

--- config/routes.rb
+++ config/routes.rb
@@ -30,16 +30,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
-
-  devise_scope :user do
-    unauthenticated :user do
-      root to: "devise/sessions#new", as: :unauthenticated_root
-    end
-
-    authenticated :user do
-      root to: "articles#index", as: :authenticated_root
-    end
-  end
+  root "welcome#index"
 
   resources :articles do
     resources :comments, :only => [:create, :destroy]
